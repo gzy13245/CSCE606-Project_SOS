53307dbdf41857d21c70e22ea225a8ef57f335f0
sfh888 <fuhaoshi@gmail.com>
2014-12-11 16:15:52 +0800

    course and teacher added

---
+++ TAMUChineseSchool_new/db/migrate/20141211063228_create_courses.rb
@@ -0,0 +1,16 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :year
+      t.string :semester
+      t.string :name
+      t.string :class_room
+      t.datatime :time
+      t.string :textbook
+      t.string :course_discription
+      t.references :teacher, index: true
+
+      t.timestamps
+    end
+  end
+end
