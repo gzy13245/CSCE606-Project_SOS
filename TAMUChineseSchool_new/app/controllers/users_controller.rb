bcbeb895ee1c3287f8629538d20c347ce265a054
stzpz <newstzpz@gmail.com>
2014-12-11 14:11:37 +0800

    Added more about users.

---
+++ TAMUChineseSchool_new/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+          :first_name, :last_name, :first_name_chn, :last_name_chn,
+          :gender, :address, :phone, :email )
+    end
+end
