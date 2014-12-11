3d0e3dea88f8ad382e324d4c428f2a97dc6ac9d8
sfh888 <fuhaoshi@gmail.com>
2014-12-11 21:45:54 +0800

    no message

--- TAMUChineseSchool_new/app/controllers/courses_controller.rb
+++ TAMUChineseSchool_new/app/controllers/courses_controller.rb
@@ -1,23 +1,27 @@
 class CoursesController < ApplicationController
   def new
+    @courses=Course.all
     @teachers=Teacher.all
   end
 
   def create
-    @course = Course.new(user_params)
+    @course = Course.new(course_params)
 
     @course.save
     redirect_to @course
   end
 
   def show
+    redirect_to action: "index"
+  end
 
+  def index
+    @courses = Course.all
   end
 
   private
-  def user_params
-    params.require(:teacher).permit(
-        :user_id)
+  def course_params
+    params.require(:course).permit(:year, :semester,:name,:class_room,:time,:textbook,:course_discription)
   end
 
 end
