3a11589134434ba238d82f0a21b72c8fb7b87685
stzpz <newstzpz@gmail.com>
2014-12-11 12:47:34 +0800

    Added users model and db.

---
+++ TAMUChineseSchool_new/db/migrate/20141211044114_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_chn
+      t.string :first_name_chn
+      t.boolean :gender
+      t.text :address
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
