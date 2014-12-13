fa33f16c534de7b999f0fc98c4d795e326a32369
stzpz <newstzpz@gmail.com>
2014-12-14 02:23:39 +0800

    Fixed user permission problem.

--- TAMUChineseSchool_final_final/config/initializers/devise_permitted_parameters.rb
+++ TAMUChineseSchool_final_final/config/initializers/devise_permitted_parameters.rb
@@ -9,7 +9,15 @@ module DevisePermittedParameters
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name_chn
+    devise_parameter_sanitizer.for(:sign_up) << :gender
+    devise_parameter_sanitizer.for(:sign_up) << :address
+    devise_parameter_sanitizer.for(:sign_up) << :phone
     devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name_chn
+    devise_parameter_sanitizer.for(:account_update) << :gender
+    devise_parameter_sanitizer.for(:account_update) << :address
+    devise_parameter_sanitizer.for(:account_update) << :phone
   end
 
 end
